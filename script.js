851a232a17ba6f8f69e1bfb4f2f83326ca40bf40
Anut.pri <124768336+anutpri@users.noreply.github.com>
2023-03-13 09:31:05 +0000

    created script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+
+
+
+// const inform = (a,b,c) => {
+//     
+//     inform('Anut','Khonkean', 'Netflix');
+//     console.log("Hi, my name is " + firstName + "I live in " + location + "and enjoy "+hobby);
+// }
+
+function inform(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy " + hobby + ".");
+  }
+  
+  inform("Anut", "Khonkean", "Netflix");
